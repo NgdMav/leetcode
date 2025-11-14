caf6033eb663c067f5176aa8cd3629b2e008bf0c
Mav <riherr00@gmail.com>
2025-11-14 20:12:06 +0300

    1342

---
+++ tutor/lc1342.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        res = 0
+        while num > 0:
+            res += 1
+            if num % 2 == 0:
+                num //= 2
+            else:
+                num -= 1
+        return res
